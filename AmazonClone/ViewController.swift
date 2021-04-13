cf79c9faebb6dba3b4bebbf514a1509665c4c141
Shanmugarajeshwaran R <tweets.shan@gmail.com>
2021-04-13 10:10:07 +0530

    Initial Commit

---
+++ AmazonClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmazonClone
+//
+//  Created by ADMIN on 13/04/21.
+//  Copyright © 2021 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
